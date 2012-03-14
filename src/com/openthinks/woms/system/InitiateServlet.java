f2c71e56a7d142cc0cf93f93a2f829c8e4716b0d
Zhang Junlong <zhangjunlong@gmail.com>
2012-03-14 17:01:30 +0800

    mod sign and init

--- src/com/openthinks/woms/system/InitiateServlet.java
+++ src/com/openthinks/woms/system/InitiateServlet.java
@@ -9,7 +9,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.openthinks.woms.account.Account;
-import com.openthinks.woms.account.AccountGroup;
+import com.openthinks.woms.account.Role;
 import com.openthinks.woms.account.service.AccountService;
 import com.openthinks.woms.system.configuration.PropertiesReader;
 
@@ -38,11 +38,12 @@ public class InitiateServlet extends HttpServlet {
 	public void init(ServletConfig config) throws ServletException {
 		// Initiate a default account
 		PropertiesReader pr = new PropertiesReader("system.properties");
-		String un = pr.getProperties("admin.username");
+		String un = pr.getProperties("admin.userid");
 		String pw = pr.getProperties("admin.password");
 
 		Account defAcc = new Account();
-		defAcc.setName(un);
+		defAcc.setUserId(un);
+		defAcc.setRole(Role.ADMIN);
 		defAcc.setPassword(pw);
 
 		try {
